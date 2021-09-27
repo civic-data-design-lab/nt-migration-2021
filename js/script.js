b1aaf67d65240c8029edad424c66d12b1ad92d31
Ashley Louie <ashleylouie27@gmail.com>
2021-09-27 10:14:31 -0400

    add template files, add motivations page

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// title case
+function titleCase(string) {
+	string = string.toLowerCase();
+	string = string.split(' ');
+	for (var i = 0; i < string.length; i++) {
+		string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1);
+	}
+	return string.join(' ');
+};
+
+// round accurately function
+function roundAccurately(number, decimalPlaces) {
+	return Number(Math.round(number + "e" + decimalPlaces) + "e-" + decimalPlaces);	
+};
+
+// convert number to string with commas
+function numberWithCommas(number) {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+// wrap multi-line text spans
+function wrapText(text, width) {
+	text.each(function() {
+		var text = d3.select(this),
+			words = text.text().split(/\s+/).reverse(),
+			word,
+			line = [],
+			lineNumber = 0,
+			lineHeight = 1.2,
+			anchor = text.attr("text-anchor"),
+			x = text.attr("x"),
+			y = text.attr("y"),
+			dy = parseFloat(text.attr("dy")),
+			tspan = text.text(null)
+				.append("tspan")
+				.attr("text-anchor", anchor)
+				.attr("x", x)
+				.attr("y", y)
+				.attr("dy", dy + "em");
+		while (word = words.pop()) {
+			line.push(word);
+			tspan.text(line.join(" "));
+			if (tspan.node().getComputedTextLength() > width) {
+				line.pop();
+				tspan.text(line.join(" "));
+				line = [word];
+				tspan = text.append("tspan")
+					.attr("text-anchor", anchor)
+					.attr("x", x)
+					.attr("y", y)
+					.attr("dy", ++lineNumber * lineHeight + dy + "em")
+					.text(word);
+			}
+		}
+	})
+};
+
+$(document).ready(function () {
+    
+})
