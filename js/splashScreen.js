70fdf49f20c254bd4ae0060395a1e293cae1dd08
niko-dellic <nikodellic@gmail.com>
2021-09-28 13:01:51 -0400

    css + index.html + new js
    CSS
    1. new css formatting for large image carousel
    2. image filters
    
    html
    1. slight changes to html to accommodate new splash image page
    2. added new JS links to index.html
    
    js
    1. created a splash page for targeting splash image and landing font
    2. crate the js for the future onClick chapter events

---
+++ js/splashScreen.js
@@ -0,0 +1,12 @@
+
+var landingFont = document.getElementById("landing-trigger")
+
+var backgroundImage = document.getElementById("image-carousel")
+
+// document.getElementById("image-carousel").style.border = "1em solid red"
+// document.getElementById("landing-trigger").style.border = "1em solid red"
+
+// landingFont.onmouseover(landingFont.style.border)
+
+console.log(landingFont)
+console.log(backgroundImage)
