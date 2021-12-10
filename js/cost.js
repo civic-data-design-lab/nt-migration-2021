ced2455beba5daaa60a6bd10e9318b99bfaf97be
Ashley Louie <ashleylouie27@gmail.com>
2021-12-10 11:55:25 -0500

    move cost buttons below viz

--- js/cost.js
+++ js/cost.js
@@ -13,16 +13,22 @@ $(document).ready(function() {
         .addTo(controller)
         .on("start", e => {
             // console.log("start");
-            // // update skip to viz shortcut button
+            // update skip to viz shortcut button
             $("#shortcut a").attr("class", "text-white");
             $("#shortcut a").find("span").html("Skip to Visualization");
             $("#shortcut a").find(".arrow").css("border-color", "#fff").removeClass("arrow-up");
+            
+            // hide toolbar buttons
+            $("#toolbar").removeClass("active");
         })
         .on("end", e => {
             // console.log("end");
-            // // update skip to top shortcut button
+            // update skip to top shortcut button
             $("#shortcut a").attr("class", "text-purple top");
             $("#shortcut a").find("span").html("Back to Top");
             $("#shortcut a").find(".arrow").css("border-color", "#662D91").addClass("arrow-up");
+
+            // show toolbar buttons
+            $("#toolbar").addClass("active");
         });
 })
